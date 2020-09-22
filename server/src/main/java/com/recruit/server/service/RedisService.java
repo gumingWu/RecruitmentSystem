b690377084481d751735a7b908c81d4b3b5f6fa6
gumingWu <15626185054@163.com>
2020-09-22 19:33:02 +0800

    新增redis配置，分出dev配置

---
+++ server/src/main/java/com/recruit/server/service/RedisService.java
@@ -0,0 +1,35 @@
+package com.recruit.server.service;
+
+import java.util.List;
+
+/**
+ * redis操作Service
+ */
+
+public interface RedisService {
+
+    /**
+     * 保存属性
+     */
+    void set(String key, Object value, long time);
+
+    /**
+     * 保存属性
+     */
+    void set(String key, Object value);
+
+    /**
+     * 获取属性
+     */
+    Object get(String key);
+
+    /**
+     * 删除属性
+     */
+    Boolean del(String key);
+
+    /**
+     * 批量删除属性
+     */
+    Long del(List<String> keys);
+}
---
+++ server/src/main/java/com/recruit/server/service/impl/RedisServiceImpl.java
@@ -0,0 +1,38 @@
+package com.recruit.server.service.impl;
+
+import com.recruit.server.service.RedisService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class RedisServiceImpl implements RedisService {
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+    @Override
+    public void set(String key, Object value, long time) {
+        redisTemplate.opsForValue().set(key, value, time, TimeUnit.SECONDS);
+    }
+
+    @Override
+    public void set(String key, Object value) {
+        redisTemplate.opsForValue().set(key, value);
+    }
+
+    @Override
+    public Object get(String key) {
+        return redisTemplate.opsForValue().get(key);
+    }
+
+    @Override
+    public Boolean del(String key) {
+        return redisTemplate.delete(key);
+    }
+
+    @Override
+    public Long del(List<String> keys) {
+        return redisTemplate.delete(keys);
+    }
+}
